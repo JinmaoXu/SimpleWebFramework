f0e85c456561878fe6b43e9cd9a01003706a90b8
jinmaoxu <xujinmao95@163.com>
2017-10-03 21:28:19 +0800

    编写了ClassUtil类用于加载类以及获取某个文件夹下面的所有类。

--- src/main/java/com/framework/config/ConfigHelper.java
+++ src/main/java/com/framework/config/ConfigHelper.java
@@ -10,14 +10,66 @@ import java.util.Properties;
  * @since 1.0.0
  */
 public class ConfigHelper {
-    //加载配置文件
+    // 加载配置文件
     private  static final Properties CONFIG_PROPERTIES = PropsUtil.loadProps(ConfigConstant.CONFIG_FILE);
+    // 默认的JSP路径
+    private static String DEFAULT_JSP_PATH = "/WEB-INF/view/";
+    private static String DEFAULT_APP_ASSET_PATH = "/asset/";
 
     /**
-     * 获取JDBD驱动
+     * 从配置文件中获取JDBD驱动
      * @return
      */
     public static String getJdbcDriver(){
         return PropsUtil.getString(CONFIG_PROPERTIES,ConfigConstant.JDBC_DRIVER);
     }
+
+    /**
+     * 从配置文件中获取URL
+     * @return
+     */
+    public static String getJdbcUrl() {
+        return PropsUtil.getString(CONFIG_PROPERTIES, ConfigConstant.JDBC_URL);
+    }
+
+    /**
+     * 从配置文件中获取数据库用户名
+     * @return
+     */
+    public static String getJdbcUsername() {
+        return PropsUtil.getString(CONFIG_PROPERTIES, ConfigConstant.JDBC_USERNAME);
+    }
+
+    /**
+     * 从配置文件中获取数据库的密码
+     * @return
+     */
+    public static String getJdbcPassword() {
+        return PropsUtil.getString(CONFIG_PROPERTIES, ConfigConstant.JDBC_PASSWORD);
+    }
+
+    /**
+     * 获取应用基础包名
+     * @return
+     */
+    public static String getAppBasePackage() {
+        return PropsUtil.getString(CONFIG_PROPERTIES, ConfigConstant.APP_BASE_PACKAGE);
+    }
+
+    /**
+     * 获取应用JSP路径
+     * @return
+     */
+    public static String getAppJspPath() {
+        return PropsUtil.getString(CONFIG_PROPERTIES, ConfigConstant.APP_BASE_PACKAGE, DEFAULT_JSP_PATH);
+    }
+
+    /**
+     * 获取应用静态资源路径
+     * @return
+     */
+    public static String getAppAssetPath() {
+        return PropsUtil.getString(CONFIG_PROPERTIES, ConfigConstant.APP_BASE_PACKAGE, DEFAULT_APP_ASSET_PATH);
+    }
+
 }
---
+++ src/main/java/com/framework/util/ClassUtil.java
@@ -0,0 +1,140 @@
+package com.framework.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+
+/**
+ * 与类操作相关的工具类、如获取类加载器、加载类、获取指定包名下的所以类
+ * @author jinmaoxu
+ * @since 1.0.0
+ */
+public class ClassUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClassUtil.class);
+
+    /**
+     * 获取类加载器
+     * @return
+     */
+    public static ClassLoader getClassLoader(){
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    /**
+     * 通过文件名加载一个类
+     * @param className
+     * @param isInitialized
+     * @return
+     */
+    public static Class<?> loadClass(String className, boolean isInitialized) {
+        Class<?> cls;
+        try {
+            cls = Class.forName(className, isInitialized, getClassLoader());
+        } catch (ClassNotFoundException e){
+            LOGGER.error("can not load this class ", e);
+            throw new RuntimeException(e);
+        }
+        return cls;
+    }
+
+    /**
+     * 获取指定包名下的所有类
+     * @param packageName
+     * @return
+     */
+    public static Set<Class<?>> getClassSet(String packageName) {
+        Set<Class<?>>  classSet = new HashSet<>();
+        try {
+            // 枚举类型
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (url != null) {
+                    //获取文件的后缀名
+                    String protocol = url.getProtocol();
+                    if (protocol.equals("file")) {
+                        String packagePath = url.getPath().replaceAll("%20", "");
+                        addClass(classSet, packagePath, packageName);
+                    } else if (protocol.equals("jar")) {
+                        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                        if (jarURLConnection != null) {
+                            JarFile jarFile = jarURLConnection.getJarFile();
+                            if (jarFile != null) {
+                                Enumeration<JarEntry> jarEntries = jarFile.entries();
+                                while (jarEntries.hasMoreElements()) {
+                                    JarEntry jarEntry = jarEntries.nextElement();
+                                    String jarEntryName = jarEntry.getName();
+                                    if (jarEntryName.endsWith(".class")) {
+                                        String className = jarEntryName.substring(0, jarEntryName.lastIndexOf(".")).replaceAll("/", ".");
+                                        doAddClass(classSet, className);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }catch (IOException e) {
+            LOGGER.error("can not find this package", e);
+        }
+        return classSet;
+    }
+
+    /**
+     * 往集合中添加一个类
+     * @param classSet
+     * @param packagePath
+     * @param packageName
+     */
+    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
+        // 筛选出.class和文件夹
+        File[] files = new File(packagePath).listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                boolean judge = file.isFile() && file.getName().endsWith(".class") || file.isDirectory();
+                return judge;
+            }
+        });
+        for (File file : files){
+            String fileName = file.getName();
+            if(file.isFile()){
+                String className = fileName.substring(0, fileName.lastIndexOf("."));
+                if (StringUtil.isNotEmpty(packageName))
+                    className = packageName + "." + className;
+                doAddClass(classSet, className);
+            } else {
+                String subPackagePath = fileName;
+                if (StringUtil.isNotEmpty(packagePath)) {
+                    subPackagePath = packagePath + "/" + subPackagePath;
+                }
+                String subPackageName = fileName;
+                if (StringUtil.isNotEmpty(packageName)) {
+                    subPackageName = packageName + "." + subPackageName;
+                }
+                addClass(classSet, subPackagePath, subPackageName);
+            }
+        }
+    }
+
+    /**
+     * 执行添加类操作
+     * @param classSet
+     * @param className
+     */
+    private static void doAddClass(Set<Class<?>> classSet, String className) {
+        Class<?> cls = loadClass(className, false);
+        classSet.add(cls);
+    }
+}
